d56b85be8f2f018d0b67d96765fb7ff0569b093a
Rodel <lentodowner@gmail.com>
2023-11-14 02:12:37 -0500

    Added register

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,22 +5,25 @@ import { StyleSheet } from 'react-native';
 
 export default function LoginPage() {
     return (
-            <ImageBackground style={styled.container} resizeMode="cover" imageStyle={{height: 500}} source={require("../../assets/jxB9GUOHTf2.webp")}>
-                    <Text style={styled.text}>Log in with your Instagram account</Text>
-                    <TextInput style={styled.textInput} type="text" placeholder={"Username, phone or email"} />
-                    <TextInput style={styled.textInput} type="text" placeholder={"Password"} />
-                    <Pressable style={styled.loginButton}><Text style={styled.loginButtonText}>Log In</Text></Pressable>
-                    <Pressable style={styled.forgotPassword}><Text>Forgot password?</Text></Pressable>
-                    <Text>----------------or-------------------</Text>
-                    <Pressable style={styled.instagramLoginButton}><Text>Continue with Instagram</Text></Pressable>
-                    <View style={styled.footer}>
-                    <Text>©2023</Text>
-                    <Text> Threads terms</Text>
-                    <Text> Privacy policy</Text>
-                    <Text> Cookies</Text>
-                    <Text> Report a problem</Text>
-                    </View>
-            </ImageBackground >
+        <ImageBackground style={styled.container} resizeMode="cover" imageStyle={{ height: 700 }} source={require("../../assets/jxB9GUOHTf2.webp")}>
+            <Text style={styled.text}>Log in with your Instagram account</Text>
+            <TextInput style={styled.textInput} type="text" placeholder={"Username, phone or email"} />
+            <TextInput style={styled.textInput} type="text" placeholder={"Password"} />
+            <Pressable style={styled.loginButton}><Text style={styled.loginButtonText}>Log In</Text></Pressable>
+            <View style={{ flexDirection: "row" }}>
+                <Pressable style={styled.forgotPassword}><Text>Forgot password?</Text></Pressable>
+                <Pressable style={styled.forgotPassword}><Text>                          Register</Text></Pressable>
+            </View>
+            <Text>----------------or-------------------</Text>
+            <Pressable style={styled.instagramLoginButton}><Text>Continue with Instagram</Text></Pressable>
+            <View style={styled.footer}>
+                <Text>©2023</Text>
+                <Text> Threads terms</Text>
+                <Text> Privacy policy</Text>
+                <Text> Cookies</Text>
+                <Text> Report a problem</Text>
+            </View>
+        </ImageBackground >
     )
 }
 
@@ -34,7 +37,6 @@ const styled = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        height: "100%",
     },
     text: {
         fontStyle: 'bold',
@@ -52,8 +54,8 @@ const styled = StyleSheet.create({
         color: "rgb(243,245,247)",
         backgroundColor: "rgb(30, 30, 30)",
         width: 400,
-        paddingTop:30,
-        paddingBottom:30,
+        paddingTop: 30,
+        paddingBottom: 30,
         marginTop: 5,
     },
     loginButton: {
@@ -63,7 +65,7 @@ const styled = StyleSheet.create({
         borderWidth: 1,
         borderColor: "rgba(0, 0 , 0, 0.4)",
         borderRadius: 5,
-        paddingTop:20,
+        paddingTop: 20,
         paddingBottom: 40,
         marginTop: 5,
         textAlign: "center",
@@ -77,19 +79,19 @@ const styled = StyleSheet.create({
         marginBottom: 10,
         fontSize: 14,
     },
-    instagramLoginButton:{
+    instagramLoginButton: {
         width: 400,
         height: 40,
         padding: 10,
         borderWidth: 1,
         borderColor: "rgba(0, 0 , 0, 0.4)",
         borderRadius: 5,
-        paddingTop:20,
+        paddingTop: 20,
         paddingBottom: 40,
         marginTop: 20,
         marginBottom: 20,
     },
-    footer:{
+    footer: {
         flexDirection: "row",
     }
 });
