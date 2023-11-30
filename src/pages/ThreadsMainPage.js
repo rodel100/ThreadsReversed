8c73ece1781aec82f20f2e0e1ff527edf2397cb9
Rodel <lentodowner@gmail.com>
2023-11-30 14:06:23 -0500

    Navigation begun

--- src/pages/ThreadsMainPage.js
+++ src/pages/ThreadsMainPage.js
@@ -22,8 +22,9 @@ export default function ThreadsMainPage() {
                         </Svg>
                     </Pressable>
                     <Pressable>
-                        <Svg width={20} height={20} fill="none" stroke="black" stroke-linejoin="round" stroke-width="2">
-                            <Path d="M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z"/>
+                        <Svg width={20} height={20} fill={'none'} strokeLinejoin={"round"} strokeWidth={"2"}>
+                            <Path d="M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z" fill="none"
+                                  stroke="currentColor" strokeLinejoin="round" width="50"/>
                         </Svg>
                     </Pressable>
                     <Pressable>
@@ -33,11 +34,11 @@ export default function ThreadsMainPage() {
                         </Svg>
                     </Pressable>
                     <Pressable>
-                        <Svg width={20} height={20} fill="none" stroke="black" stroke-linejoin="round" stroke-width="2">
-                            <Line fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2" x1="22"
+                        <Svg width={20} height={20} fill="none" stroke="black" strokeLinejoin="round" strokeWidth="2">
+                            <Line fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth="2" x1="22"
                                   x2="9.218" y1="3" y2="10.083"/>
                             <Polygon fill="none" points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334"
-                                     stroke="currentColor" stroke-linejoin="round" stroke-width="2"/>
+                                     stroke="currentColor" strokeLinejoin="round" strokeWidth="2"/>
                         </Svg>
                     </Pressable>
                 </View>
@@ -50,12 +51,37 @@ export default function ThreadsMainPage() {
     );
     return (
         <View style={styled.screenLayer}>
-            <Pressable><Image style={styled.logoImage}
-                              source={require("../../assets/threads-app-icon.webp")}></Image></Pressable>
+            <View>
+                <Pressable><Image style={styled.logoImage}
+                                  source={require("../../assets/threads-app-icon.webp")}></Image></Pressable>
+                <View>
+                    <Pressable>
+                        <Svg width={20} height={20} fill="none" stroke="black" strokeLinejoin="round" strokeWidth="2">
+                            <Path
+                                d={"M2.25 12.8855V20.7497C2.25 21.8543 3.14543 22.7497 4.25 22.7497H8.25C8.52614 22.7497 8.75 22.5259 8.75 22.2497V17.6822V17.4997C8.75 15.1525 10.6528 13.2497 13 13.2497C15.3472 13.2497 17.25 15.1525 17.25 17.4997V17.6822V22.2497C17.25 22.5259 17.4739 22.7497 17.75 22.7497H21.75C22.8546 22.7497 23.75 21.8543 23.75 20.7497V12.8855C23.75 11.3765 23.0685 9.94815 21.8954 8.99883L16.1454 4.3454C14.3112 2.86095 11.6888 2.86095 9.85455 4.3454L4.10455 8.99883C2.93153 9.94815 2.25 11.3765 2.25 12.8855Z"}
+                                fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth="2"/>
+                        </Svg>
+                    </Pressable>
+                    <Pressable>
+                        <Svg width={20} height={20} fill="none" stroke="black" strokeLinejoin="round" strokeWidth="2">
+                            <Path
+                                d={"M3.5 11.5C3.5 7.08172 7.08172 3.5 11.5 3.5C15.9183 3.5 19.5 7.08172 19.5 11.5C19.5 15.9183 15.9183 19.5 11.5 19.5C7.08172 19.5 3.5 15.9183 3.5 11.5ZM11.5 1C5.70101 1 1 5.70101 1 11.5C1 17.299 5.70101 22 11.5 22C13.949 22 16.2023 21.1615 17.9883 19.756L22.3661 24.1339C22.8543 24.622 23.6457 24.622 24.1339 24.1339C24.622 23.6457 24.622 22.8543 24.1339 22.3661L19.756 17.9883C21.1615 16.2023 22 13.949 22 11.5C22 5.70101 17.299 1 11.5 1Z"}
+                                fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth="2"/>
+                        </Svg>
+                    </Pressable>
+                    <Pressable>
+                        <Svg width={20} height={20} fill="none" stroke="black" strokeLinejoin="round" strokeWidth="2">
+                            <Path d={"M22.75 13L22.75 13.15C22.75 16.5103 22.75 18.1905 22.096 19.4739C21.5208 20.6029 20.6029 21.5208 19.4739 22.096C18.1905 22.75 16.5103 22.75 13.15 22.75L12.85 22.75C9.48969 22.75 7.80953 22.75 6.52606 22.096C5.39708 21.5208 4.4792 20.6029 3.90396 19.4739C3.25 18.1905 3.25 16.5103 3.25 13.15L3.25 12.85C3.25 9.48968 3.25 7.80953 3.90396 6.52606C4.4792 5.39708 5.39708 4.4792 6.52606 3.90396C7.80953 3.25 9.48968 3.25 12.85 3.25L13 3.25"}
+                                    fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth="2"/>
+                        </Svg>
+                    </Pressable>
+                </View>
+            </View>
             <FlatList
                 data={dataThreadsSampleData}
                 renderItem={({item}) => <ThreadItem thread={item}/>}
                 keyExtractor={(item) => item.id}
+                showsVerticalScrollIndicator={false}
             />
         </View>
     )
@@ -67,18 +93,20 @@ const styled = StyleSheet.create({
         height: 20,
     },
     mainImage: {
-        maxHeight: Dimensions.get('window').height,
-        maxWidth: Dimensions.get('window').width + 30,
-        minWidth: 300,
-        minHeight: 300,
+        width: 60,
+        height: 90,
         borderRadius: 10,
-        resizeMode: "contain",
         marginRight: 10,
     },
     outerLayer: {
         borderRadius: 10,
         borderWidth: 1,
         flexDirection: "row",
+        maxHeight: Dimensions.get('window').height - 20,
+        maxWidth: Dimensions.get('window').width - 20,
+        overflow: "hidden",
+        width: Dimensions.get('window').width - 800,
+        height: Dimensions.get('window').height - 800,
     },
     screenLayer: {
         flex: 1,
